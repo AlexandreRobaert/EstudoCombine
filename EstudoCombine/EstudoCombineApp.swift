e4344905d70c4f96092a0e15fa5017977d2349f6
Alexandre Robaert <robaert.developer.com>
2024-10-13 19:39:52 -0300

    Initial Commit

---
+++ EstudoCombine/EstudoCombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  EstudoCombineApp.swift
+//  EstudoCombine
+//
+//  Created by Alexandre Robaert on 13/10/24.
+//
+
+import SwiftUI
+
+@main
+struct EstudoCombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
